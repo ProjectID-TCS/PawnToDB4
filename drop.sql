39f59575464532e36c209755b212469f50801701
Michał Hoffmann <42720407+Crackhoff@users.noreply.github.com>
2023-04-29 20:00:14 +0200

    update drop to work on schema

--- drop.sql
+++ drop.sql
@@ -1,77 +1,82 @@
-CREATE  TABLE pawntodb4.zawodnicy ( 
-	id                   integer  NOT NULL  ,
-	imie                 varchar[]    ,
-	nazwisko             varchar[]    ,
-	grupa                integer    ,
-	elo                  integer    ,
-	maxelo               integer    ,
-	CONSTRAINT pk_zawodnicy PRIMARY KEY ( id ),
-	CONSTRAINT unq_zawodnicy_grupa UNIQUE ( grupa ) 
- );
+DROP SCHEMA IF EXISTS PTDB4 CASCADE;
 
-ALTER TABLE pawntodb4.zawodnicy ADD CONSTRAINT fk_zawodnicy_partie FOREIGN KEY ( id ) REFERENCES pawntodb4.partie( białe );
+CREATE SCHEMA PTDB4;
 
-ALTER TABLE pawntodb4.zawodnicy ADD CONSTRAINT fk_zawodnicy_partie_0 FOREIGN KEY ( id ) REFERENCES pawntodb4.partie( czarne );
-CREATE  TABLE pawntodb4.partie ( 
-	id                   integer  NOT NULL  ,
-	białe                integer  NOT NULL  ,
-	czarne               integer  NOT NULL  ,
-	id_tur               integer    ,
-	wynik                boolean  NOT NULL  ,
-	"data"               date    ,
-	CONSTRAINT unq_partie_białe UNIQUE ( białe ) ,
-	CONSTRAINT unq_partie_czarne UNIQUE ( czarne ) ,
-	CONSTRAINT pk_partie PRIMARY KEY ( id ),
-	CONSTRAINT unq_partie_id_tur UNIQUE ( id_tur ) 
- );
-CREATE  TABLE pawntodb4.turnieje ( 
-	id_tur               integer  NOT NULL  ,
-	nazwa                varchar[]  NOT NULL  ,
-	typ                  integer  NOT NULL  ,
-	miejsce              integer    ,
-	początek             date  NOT NULL  ,
-	koniec               date  NOT NULL  ,
-	CONSTRAINT pk_turnieje PRIMARY KEY ( id_tur ),
-	CONSTRAINT unq_turnieje_typ UNIQUE ( typ ) ,
-	CONSTRAINT unq_turnieje_miejsce UNIQUE ( miejsce ) 
- );
-CREATE  TABLE pawntodb4.miejsca ( 
-	id                   integer  NOT NULL  ,
-	miasto               integer    ,
-	CONSTRAINT pk_miejsca PRIMARY KEY ( id )
- );
+CREATE TYPE PTDB4.match_result AS ENUM('white', 'black', 'draw');
 
-ALTER TABLE pawntodb4.miejsca ADD CONSTRAINT fk_miejsca_turnieje FOREIGN KEY ( id ) REFERENCES pawntodb4.turnieje( miejsce );
-CREATE  TABLE pawntodb4.zaw_tur ( 
-	id_zaw               integer  NOT NULL  ,
-	id_tur               integer  NOT NULL  
- );
+CREATE TABLE PTDB4.groups(
+    id integer PRIMARY KEY,
+    group_name varchar UNIQUE NOT NULL
+);
+CREATE TABLE PTDB4.players(
+    id integer PRIMARY KEY,
+    first_name varchar(40) NOT NULL, 
+    last_name varchar(40) NOT NULL,
+    group_id integer REFERENCES ptdb4.groups,
+    elo integer check(elo > 0 and elo < 5000),
+    max_elo integer check(max_elo >= elo)
+);
 
-ALTER TABLE pawntodb4.zaw_tur ADD CONSTRAINT fk_zaw_tur_zawodnicy FOREIGN KEY ( id_zaw ) REFERENCES pawntodb4.zawodnicy( id );
+CREATE TABLE PTDB4.types(
+    id integer PRIMARY KEY,
+    name varchar(40) NOT NULL,
+    number_of_players integer NOT NULL
+);
+CREATE TABLE PTDB4.places(
+    id integer PRIMARY KEY,
+    city varchar(50) NOT NULL,
+    street varchar(100),
+    street_number integer
+);
 
-ALTER TABLE pawntodb4.zaw_tur ADD CONSTRAINT fk_zaw_tur_turnieje FOREIGN KEY ( id_tur ) REFERENCES pawntodb4.turnieje( id_tur );
-CREATE  TABLE pawntodb4.grupy ( 
-	id                   integer  NOT NULL  ,
-	grupa                varchar[]    ,
-	CONSTRAINT pk_grupy PRIMARY KEY ( id )
- );
+CREATE TABLE PTDB4.tournaments(
+    id integer PRIMARY KEY,
+    "name" varchar NOT NULL,
+    "type" integer NOT NULL REFERENCES ptdb4.types,
+    place integer REFERENCES ptdb4.places,
+    "start_date" date NOT NULL,
+    "end_date" date NOT NULL,
+    check(start_date < end_date)
+);
+CREATE TABLE PTDB4.move_record(
+    id integer PRIMARY KEY,
+    record varchar NOT NULL
+);
 
-ALTER TABLE pawntodb4.grupy ADD CONSTRAINT fk_grupy_zawodnicy FOREIGN KEY ( id ) REFERENCES pawntodb4.zawodnicy( grupa );
-CREATE  TABLE pawntodb4.typ ( 
-	id                   integer  NOT NULL  ,
-	nazwa                varchar[]    ,
-	liczba_zaw           integer    ,
-	CONSTRAINT pk_typ PRIMARY KEY ( id )
- );
+CREATE TABLE PTDB4.openings(
+    id integer PRIMARY KEY,
+    first_move varchar(3),
+    name varchar(100) UNIQUE NOT NULL
+);
 
-ALTER TABLE pawntodb4.typ ADD CONSTRAINT fk_typ_turnieje FOREIGN KEY ( id ) REFERENCES pawntodb4.turnieje( typ );
-CREATE  TABLE pawntodb4.par_tur ( 
-	id_par               integer  NOT NULL  ,
-	id_tur               integer  NOT NULL  ,
-	szczebel             integer  NOT NULL  ,
-	CONSTRAINT unq_par_tur_id_par UNIQUE ( id_par ) 
- );
 
-ALTER TABLE pawntodb4.par_tur ADD CONSTRAINT fk_par_tur_turnieje FOREIGN KEY ( id_tur ) REFERENCES pawntodb4.turnieje( id_tur );
+CREATE TABLE PTDB4.game_record(
+    id integer PRIMARY KEY,
+    id_record integer REFERENCES ptdb4.move_record,
+    id_opening integer REFERENCES ptdb4.openings,
+    game_result match_result NOT NULL,
+    ending varchar,
+    UNIQUE(id_record)
+);
 
-ALTER TABLE pawntodb4.par_tur ADD CONSTRAINT fk_par_tur_partie FOREIGN KEY ( id_par ) REFERENCES pawntodb4.partie( id );
+
+CREATE TABLE PTDB4.pairings(
+    id integer PRIMARY KEY,
+    white integer NOT NULL REFERENCES ptdb4.players,
+    black integer NOT NULL REFERENCES ptdb4.players,
+    tournament_id integer REFERENCES ptdb4.tournaments,
+    "result" match_result NOT NULL,
+    "date"   date,
+    id_record integer REFERENCES ptdb4.game_record
+);
+
+CREATE TABLE PTDB4.pairing_tournament(
+    pairing_id integer UNIQUE NOT NULL REFERENCES ptdb4.pairings,
+    tournament_id integer NOT NULL REFERENCES ptdb4.tournaments,
+    rank integer
+);
+
+CREATE TABLE PTDB4.player_tournament(
+    player_id integer NOT NULL REFERENCES ptdb4.players,
+    tournament_id integer NOT NULL REFERENCES ptdb4.tournaments
+);
